697f26ccaa45d4feefb0399cd1f1f106a1153346
Gabe Wachob <gwachob@wachob.com>
2008-11-22 16:09:38 -0800

    skeletal files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+ 
+# ...
+ 
+from distutils.core import setup
+ 
+setup(name='pydiscovery',
+      version='0.1',
+      description='pydiscovery: XRD(S) parsing and discovery',
+      author_email='gwachob@wachob.com',
+      url=' http://github.com/gwachob/pydiscovery',
+      packages=['discovery', ],
+      long_description="Discover things from XRD(S) and profit.",
+      license="Apache 2.0",
+      platforms=["any"],
+     )
+ 
